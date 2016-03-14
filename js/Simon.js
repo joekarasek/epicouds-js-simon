e2b0bcb5a0b7e793efc758a8428a2c3933b09a32
Joe Karasek and Mary Warrington <joseph.karasek@gmail.com>
2016-03-14 13:26:51 -0700

    add files for Simon object

---
+++ js/Simon.js
@@ -0,0 +1,9 @@
+function Simon() {
+  // This constructs Simon
+}
+
+Simon.prototype.anAction = function () {
+  // This is a method
+};
+
+exports.Simon = Simon;
---
+++ tmp/allConcat.js
@@ -0,0 +1,4 @@
+// Require statements to pull in other resources
+var Simon = require('./../js/Simon.js').Simon;
+
+// Document Ready Jquery for page triggers
